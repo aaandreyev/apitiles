23f2d71393bf3058030160f9d54e6908fdf48a28
aaandreyev <aandreyev.aanton@gmail.com>
2015-11-17 04:50:33 +0100

    scafolded

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apitilesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the apitilesApp
+ */
+angular.module('apitilesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
